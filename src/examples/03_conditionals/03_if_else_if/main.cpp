58536cd23e54a4b557a6cfbac3660133658eab6e
Kimberly martinez <143145299+Kimmy218@users.noreply.github.com>
2023-09-17 19:47:27 +0000

    If else iff

--- src/examples/03_conditionals/03_if_else_if/main.cpp
+++ src/examples/03_conditionals/03_if_else_if/main.cpp
@@ -1,5 +1,5 @@
 //write include statements
-#include "if_else.h"
+#include "if_else_if.h"
 #include<iostream>
 
 //write cin and cout using statements
@@ -12,7 +12,13 @@ Pass the year to get_generation function and display function result.
 */
 int main() 
 {
-	
+	auto year = 0;
+	cout<<"Enter a year: "; //asking user to input year like a print statement in python
+	cin>>year;  //telling the system what user inputs is assigning it to the year result
+
+	auto result = get_generation(year); //year result is and pulling from original funtion to determine Gen
+
+	cout<<"The generation is " <<result<<"\n";
 	
 	return 0;
 }
